746b3b85c63b6a2f740866e31d20927a2de53eca
Minno <>
2019-06-09 12:27:12 -0500

    Update deployment process to use staging environment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-cargo make all_release
-aws s3 cp . s3://fehsimseed/fehstatsim --recursive --exclude "*" --include "*.html" --include "*.js" --include "pkg/package.js" --include "style.css" --profile S3
-aws s3 cp ./pkg/package_bg.wasm s3://fehsimseed/fehstatsim/pkg/package_bg.wasm --content-type application/wasm --profile S3
+aws s3 sync s3://fehsimseed-staging s3://fehsimseed --profile S3
+aws cloudfront create-invalidation --distribution-id EC9ULX41LPJXL --paths "/*" --profile S3
